da8dc55449394476f35e0dfa3a164d7555c3ef0b
Konrad Wajda <konrad.wajda@interia.pl>
2017-10-17 18:46:33 +0200

    MrauMrauChan functions

--- date.cpp
+++ date.cpp
@@ -1,1 +1,18 @@
 #include "date.h"
+Date::Date(int year, int month, int day)
+{
+    int y;
+    if(month==1)
+        y=0;
+    else
+        y=month-2;
+    unixTime=((year-1970)*365+day+monthDate[y])*24*60*60;
+}
+
+const Date operator-(const Date& D,int n)
+{
+    Date newDate;
+    newDate.unixTime=D.unixTime-(n*24*60*60);
+    return newDate;
+
+}
