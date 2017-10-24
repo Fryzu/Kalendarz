c3583b22826a9fd37b8db058b96c7c85345ba413
Fryzu <bartek.fryz@gmail.com>
2017-10-24 14:33:33 +0200

    format

--- src/date.cpp
+++ src/date.cpp
@@ -3,14 +3,14 @@
 
 //Metody Bartka
 
-Date& Date::operator=(const Date& toCopy)
+Date &Date::operator=(const Date &toCopy)
 {
     this->unixDays = toCopy.unixDays;
 
     return *this;
 }
 
-const Date operator+(int dateInteger, const Date& dateObject)
+const Date operator+(int dateInteger, const Date &dateObject)
 {
     Date result;
     result.unixDays = dateObject.unixDays + dateInteger;
@@ -18,27 +18,29 @@ const Date operator+(int dateInteger, const Date& dateObject)
     return result;
 }
 
-std::ostream& operator<<(std::ostream& os, const Date& dateObject)
+std::ostream &operator<<(std::ostream &os, const Date &dateObject)
 {
     os << dateObject.unixDays << std::endl;
 
     return os;
 }
 
-string ComputerDateFormatter::format(Date& date)
+string ComputerDateFormatter::format(Date &date)
 {
     string result;
-    result << date.unixDays/365 << "-";
+    result << date.unixDays / 365 << "-";
 
-	int dateDays = date.unixDays % 365;
-    for(int i = 0; i < 12; i++)
+    int dateDays = date.unixDays % 365;
+    for (int i = 0; i < 12; i++)
     {
-		if(dateDays < monthDate[i])
-		{
-			result << i + 1 << "-";
-			if(i == 1)	result << dateDays; 
-			else	result << dateDays - monthDate[i-1];
-		}
+        if (dateDays < monthDate[i])
+        {
+            result << i + 1 << "-";
+            if (i == 1)
+                result << dateDays;
+            else
+                result << dateDays - monthDate[i - 1];
+        }
     }
 }
 
@@ -47,89 +49,82 @@ string ComputerDateFormatter::format(Date& date)
 Date::Date(int year, int month, int day)
 {
     int y;
-    if(month==1)
-        y=0;
+    if (month == 1)
+        y = 0;
     else
-        y=month-2;
-    unixDays=((year-1970)*365+day+monthDate[y]);
+        y = month - 2;
+    unixDays = ((year - 1970) * 365 + day + monthDate[y]);
 }
 
-const Date operator-(const Date& D,int n)
+const Date operator-(const Date &D, int n)
 {
     Date newDate;
-    newDate.unixDays=D.unixDays-n;
-    if (newDate.unixDays<0)
-        newDate.unixDays=0;
+    newDate.unixDays = D.unixDays - n;
+    if (newDate.unixDays < 0)
+        newDate.unixDays = 0;
     return newDate;
-
 }
 
 //Metody Pietra
 
-Date& Date::operator+=(const int days)
+Date &Date::operator+=(const int days)
 {
-    unixDays=unixDays+days;
+    unixDays = unixDays + days;
     return *this;
 }
-Date& Date::operator-=(const int days)
+Date &Date::operator-=(const int days)
 {
-	unixDays=unixDays-days;
+    unixDays = unixDays - days;
     return *this;
 }
- const Date operator+(const Date& prevDate, int days)
+const Date operator+(const Date &prevDate, int days)
 {
     Date newDate;
-    newDate.unixDays=prevDate.unixDays+days;
+    newDate.unixDays = prevDate.unixDays + days;
     return newDate;
 }
-bool operator==(const Date& D1, const Date& D2);
+bool operator==(const Date &D1, const Date &D2);
 {
 
-    if (D1.unixDays!=D2.unixDays) return 0
-    else return 1
+    if (D1.unixDays != D2.unixDays)
+        return 0 else return 1
 }
- bool operator!=(const Date&, const Date&)
+bool operator!=(const Date &, const Date &)
 {
-	    if (D1.unixDays!=D2.unixDays) return 1
-	    else return 0
+    if (D1.unixDays != D2.unixDays)
+        return 1 else return 0
 }
 
+string GermanDateFormatter::format(Date &date)
+{
+    string result;
+    const int yearLength = 365;
 
-
-string GermanDateFormatter::format(Date& date)
-    {
-      string result;
-      const int yearLength = 365;
-
-    unsigned int years = 1970+((unixDays-unixDays%yearLength)/yearLength)
-    string  year= '.'+to_string(years);
+    unsigned int years = 1970 + ((unixDays - unixDays % yearLength) / yearLength)
+                                    string year = '.' + to_string(years);
 
     int months;
 
     string month;
 
-    for(int i=1;i<13;i++)
+    for (int i = 1; i < 13; i++)
     {
-        if (date.unixDays-yearLength*years)<=monthDate[i])
-        {
-                        months=i;
-                        break;
-
-        }
-
-
+        if (date.unixDays - yearLength * years)<=monthDate[i])
+            {
+                months = i;
+                break;
+            }
     }
-    if (months<10) month='.0'+to_string(months)
-            else month='.'+to_string(months);
+    if (months < 10)
+        month = '.0' + to_string(months) else month = '.' + to_string(months);
 
-    unsigned int days=date.unixDays-((years-1970)*yearLength+monthDate[i-1]);
+    unsigned int days = date.unixDays - ((years - 1970) * yearLength + monthDate[i - 1]);
 
     string day;
-     if (days<10) day='0'+to_string(days)
-            else day=to_string(days);
-    result=day+month+year;
+    if (days < 10)
+        day = '0' + to_string(days) else day = to_string(days);
+    result = day + month + year;
     return result;
-    }
-
-
-};
+}
+}
+;
--- src/date.h
+++ src/date.h
@@ -7,42 +7,42 @@
 
 class Date
 {
-    private:
-        unsigned int unixDays;
-		static const int monthDate[12] = {31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+  private:
+    unsigned int unixDays;
+    static const int monthDate[12] = {31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
 
-    public:
-        Date(int year,int month,int day);//MrauMrauChan
-        Date(unsigned int time = 0) : unixDays(time-time%86400){};
+  public:
+    Date(int year, int month, int day); //MrauMrauChan
+    Date(unsigned int time = 0) : unixDays(time - time % 86400){};
 
-        Date& operator+=(const int); //Pietro
-        Date& operator-=(const int); //Pietro
-        Date& operator=(const Date&); //Fryzu
+    Date &operator+=(const int);   //Pietro
+    Date &operator-=(const int);   //Pietro
+    Date &operator=(const Date &); //Fryzu
 
-        friend const Date operator+(int, const Date&); //n+D Fryzu
-        friend const Date operator+(const Date&, int); //D+n Pietro
-        friend const Date operator-(const Date&, int); //D-n MrauMrauChan
-        friend bool operator==(const Date&, const Date&); //Pietro
-        friend bool operator!=(const Date&, const Date&); //Pietro
-        friend std::ostream& operator<<(std::ostream&, const Date&); //Fryzu
+    friend const Date operator+(int, const Date &);                //n+D Fryzu
+    friend const Date operator+(const Date &, int);                //D+n Pietro
+    friend const Date operator-(const Date &, int);                //D-n MrauMrauChan
+    friend bool operator==(const Date &, const Date &);            //Pietro
+    friend bool operator!=(const Date &, const Date &);            //Pietro
+    friend std::ostream &operator<<(std::ostream &, const Date &); //Fryzu
 };
 
 class DateFormatter // nie wiem do końca po co - może nie o to chodziło
 {
-    public:
-        virtual string format(Date& date) = 0;
+  public:
+    virtual string format(Date &date) = 0;
 };
 
 class ComputerDateFormatter : public DateFormatter
 {
-    public:
-        virtual string format(Date& date); //Fryzu
+  public:
+    virtual string format(Date &date); //Fryzu
 };
 
 class GermanDateFormatter : public DateFormatter
 {
-    public:
-        virtual string format(Date& date); //Pietro
+  public:
+    virtual string format(Date &date); //Pietro
 };
 
 //ComputerDateFormatter cdf;
